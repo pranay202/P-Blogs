0f875d00b80d5a62b39db19badb6ff42a9e5bce5
pranay202 <pranaykharabe20@gmail.com>
2021-10-28 23:28:04 +0530

    add more routes

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,11 +1,18 @@
 import express from 'express';
 
 // importing controller for router
-import { createPost } from '../controller/post-controller';
+import { createPost, getAllPosts, getPost, updatePost, deletePost } from '../controller/post-controller.js';
 
 const router = express.Router();
 
 router.post('/create', createPost);
     //save Data to MongoDB
 
-// export default router;
+router.get('/posts', getAllPosts);
+router.get('/post/:id', getPost);
+
+router.post('/update/:id', updatePost);
+
+router.delete('/delete/:id', deletePost);
+
+export default router;
