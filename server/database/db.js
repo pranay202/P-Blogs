c72bfbcd6a14e4aae9177273ffe99deb07166d57
pranay202 <pranaykharabe20@gmail.com>
2021-10-28 23:37:52 +0530

    connected

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ const Connection = async () => {
     try {
         const MONGO_URL = "mongodb+srv://user:Pranay2329@blogcluster.u3mtv.mongodb.net/BlogCluster?retryWrites=true&w=majority";
         await mongoose.connect(MONGO_URL, {
-            // useUnifiedTopology: true,
+            useUnifiedTopology: true,
             // useFindAndModify: false,
         });       
         console.log("Database Connection Successful!");
