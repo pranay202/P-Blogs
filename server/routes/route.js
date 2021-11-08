f2a6762730027635d61f850e34f61bdaf7408d7a
pranay202 <pranaykharabe20@gmail.com>
2021-11-08 20:38:10 +0530

    update routes

--- server/routes/route.js
+++ server/routes/route.js
@@ -2,17 +2,20 @@ import express from 'express';
 
 // importing controller for router
 import { createPost, getAllPosts, getPost, updatePost, deletePost } from '../controller/post-controller.js';
+import { uploadImage } from '../controller/image-controller.js';
+import upload from '../utils/upload.js';
 
 const router = express.Router();
 
 router.post('/create', createPost);
+router.put('/update/:id', updatePost);
+router.delete('/delete/:id', deletePost);
     //save Data to MongoDB
 
 router.get('/posts', getAllPosts);
 router.get('/post/:id', getPost);
 
-router.post('/update/:id', updatePost);
-
-router.delete('/delete/:id', deletePost);
+router.post('/file/upload', upload.single('file'), uploadImage);
+// router.get('/file/:filename', getImage);
 
 export default router;
